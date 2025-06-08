d6c006ebd7fef3ee2926bd0cbe6ed98f5c6845e2
WassimSellami <wassim.sellami@supcom.tn>
2025-06-08 14:30:14 +0200

    format data and rename variables

--- frontend/src/app/user-form/user-form.component.ts
+++ frontend/src/app/user-form/user-form.component.ts
@@ -13,29 +13,35 @@ export class UserFormComponent {
     { label: 'Thomn', value: UnitType.Thomn },
     { label: 'Page', value: UnitType.Page }
   ];
-  currentStepNumber = 1
-  steps = ['unitType', 'startUnit', 'endUnit', 'memDays', 'revDays', 'restDays', 'startDate', 'preview'];
+
+  formData = {
+    'memorizationUnit': UnitType.Thomn,
+    'startUnit': 'H1T1',
+    'endUnit': 'H15T8',
+    'memorizationDaysPerCycle': 2,
+    'revisionDaysPerCycle': 2,
+    'restDaysPerCycle': 1,
+    'startDate': '2025-06-11',
+  };
+
+  steps = ['memorizationUnit', 'startUnit', 'endUnit', 'memorizationDaysPerCycle', 'revisionDaysPerCycle', 'restDaysPerCycle', 'startDate', 'preview'];
+
   stepDescriptions: { [key: string]: string } = {
-    'unitType': 'how much do you want to memorize per day?',
+    'memorizationUnit': 'how much do you want to memorize per day?',
     'startUnit': 'Where do you want to start memorizing from?',
     'endUnit': 'Where do you want to end?',
-    'memDays': 'How many days do you want to memorize per cycle?',
-    'revDays': 'How many days do you want to revise per cycle?',
-    'restDays': 'How many days do you want to rest per cycle?',
+    'memorizationDaysPerCycle': 'How many days do you want to memorize per cycle?',
+    'revisionDaysPerCycle': 'How many days do you want to revise per cycle?',
+    'restDaysPerCycle': 'How many days do you want to rest per cycle?',
     'startDate': 'When Do you want to start your trip?',
     'preview': 'Done! Now you can generate a preview of your plan!',
   };
+
+  inputJson = {};
+  currentStepNumber = 1
   currentStep = this.steps[this.currentStepNumber - 1];
   currentStepDescription = this.stepDescriptions[this.currentStep];
-  formData = {
-    'unitType': UnitType.Thomn,
-    'startUnit': 'H1T1',
-    'endUnit': 'H15T8',
-    'memDays': 2,
-    'revDays': 2,
-    'restDays': 1,
-    'startDate': '2025-06-11',
-  };
+
   getProgress(): number {
     return Math.round(this.currentStepNumber / this.steps.length * 100);
   }
@@ -48,6 +54,7 @@ export class UserFormComponent {
     this.currentStep = this.steps[this.currentStepNumber - 1];
     this.currentStepDescription = this.stepDescriptions[this.currentStep];
   }
+
   goToPreviousStep() {
     this.currentStepNumber -= 1;
     if (this.currentStepNumber == 0) {
@@ -59,17 +66,26 @@ export class UserFormComponent {
 
   getFormattedSummary() {
     return `
-    Memorization Unit: ${UnitType[this.formData.unitType]}
+    Memorization Unit: ${UnitType[this.formData.memorizationUnit]}
     Start from: ${this.formData.startUnit}
     End at: ${this.formData.endUnit}
-    Memorization days per cycle: ${this.formData.memDays}
-    Revision days per cycle: ${this.formData.revDays}
-    Rest days per cycle: ${this.formData.restDays}
+    Memorization days per cycle: ${this.formData.memorizationDaysPerCycle}
+    Revision days per cycle: ${this.formData.revisionDaysPerCycle}
+    Rest days per cycle: ${this.formData.restDaysPerCycle}
     Start Date: ${this.formData.startDate}
   `.trim();
   }
 
   generatePreview() {
-    console.log('generating preview');
+    this.inputJson = {
+      'memorizationUnit': UnitType[this.formData.memorizationUnit],
+      'startUnit': this.formData.startUnit,
+      'endUnit': this.formData.endUnit,
+      'memorizationDaysPerCycle': this.formData.memorizationDaysPerCycle,
+      'revisionDaysPerCycle': this.formData.revisionDaysPerCycle,
+      'restDaysPerCycle': this.formData.restDaysPerCycle,
+      'startDate': this.formData.startDate
+    };
+    console.log(this.inputJson)
   }
 }
