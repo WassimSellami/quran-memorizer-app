1a2179dda9d0ad5e85a453f43d248c3f6ff1304d
WassimSellami <wassim.sellami@supcom.tn>
2025-06-21 11:13:09 +0200

    remove push notifs service

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import cors from 'cors';
 import gptRoutes from './routes/gptRoutes.js';
-import pushRoutes from './routes/pushRoutes.js';
 import emailRoutes from './routes/emailRoutes.js';
 import cron from 'node-cron';
 import { emailService } from './services/emailService.js';
@@ -12,12 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Backend is running on Render!');
-});
-
 app.use('/api/gpt', gptRoutes);
-app.use('/api/push', pushRoutes);
 app.use('/api/email', emailRoutes);
 
 cron.schedule('*/20 * * * * *', () => {
